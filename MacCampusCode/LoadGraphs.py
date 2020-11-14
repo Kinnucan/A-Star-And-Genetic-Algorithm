8311b4275c0ece9204a99c6dc41cd77b68bdda6f
Kinnucan <kinnucanr@gmail.com>
2020-11-14 10:24:28 -0600

    Working on Task 1

--- MacCampusCode/LoadGraphs.py
+++ MacCampusCode/LoadGraphs.py
@@ -10,3 +10,8 @@ import MapGraph
 
 macGraph = MapGraph.readMapFile("macGraph.txt")
 print("Read in Macalester graph")
+
+print("Neighbors of node 85:")
+print(macGraph.getNeighbors(85))
+print("Straight-line distance between 85 and 86:", macGraph.heuristicDist(85, 86))
+print("Straight-line distance between 85 and 12:", macGraph.heuristicDist(85, 12))

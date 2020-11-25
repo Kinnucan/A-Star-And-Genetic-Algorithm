0609707ac159ac650a9d432fae80727cc34bb1ed
lprehn <lprehn@macalester.edu>
2020-11-25 16:46:29 -0600

    genetic algorithm almost done

--- ALifeSim/LocalSearchSolver.py
+++ ALifeSim/LocalSearchSolver.py
@@ -83,6 +83,9 @@ class RulesetState(object):
         """Takes a ruleset and returns a new ruleset identical to the original, but with one random change."""
         randElem = random.randrange(len(self.ruleString))
         opts = self._otherSymbols(self.ruleString[randElem])
+        print(randElem)
+        print(self.ruleString)
+        print(self.ruleString[randElem])
         newElem = random.choice(opts)
         newRules = self.ruleString[:randElem] + newElem + self.ruleString[randElem+1:]
         return RulesetState(self.evalFunction, self.maxValue, newRules)
@@ -104,6 +107,30 @@ class RulesetState(object):
         for i in range(self.RULE_LEN):
             rules += random.choice(options)
         return rules
+
+    def crossover(self, otherState):
+        """Given another NQueens state, this computes a crossover point and creates
+        two new states that have been crossed over."""
+        crossPoint = random.randint(0, self.RULE_LEN)
+        if crossPoint == 0 or crossPoint == self.RULE_LEN:
+            new1 = self.copyState()
+            new2 = otherState.copyState()
+            return new1, new2
+        else:
+            newString1 = ""
+            newString2 = ""
+            newString1 = newString1 + (self.ruleString[:crossPoint])
+            newString1 = newString1 + (otherState.ruleString[crossPoint:])
+            newString2 = newString2 + (otherState.ruleString[:crossPoint])
+            newString2 = newString2 + (self.ruleString[crossPoint:])
+            new1 = RulesetState(self.evalFunction, self.maxValue, newString1)
+            new2 = RulesetState(self.evalFunction, self.maxValue, newString2)
+            return new1, new2
+
+    def copyState(self):
+        """Builds and returns a new state identical to this one"""
+        string = self.ruleString
+        return RulesetState(self.evalFunction, self.maxValue, string)
 
     def __str__(self):
         """Make a string representation of this state, for printing"""
@@ -212,18 +239,23 @@ class HillClimber(object):
 
 # TODO: Implement the Genetic Algorithm searcher modeled on the HillClimber above
 
-class GASearcher:
-    def __init__(self, startState, maxRounds=500):
-        """Sets up the starting state"""
-        self.startState = startState
-        self.maxRounds = maxRounds
-        self.maxValue = startState.getMaxValue()
-        self.currState = startState
-        # This next step is EXPENSIVE!
-        self.currValue = self.currState.getValue()
+class GASearcher(object):
+
+    def __init__(self, stateGen, popSize=30, maxGenerations=50, crossPerc=0.8, mutePerc=0.01):
+        self.stateGen = stateGen
+        self.popSize = popSize
+        self.maxGenerations = maxGenerations
+        self.crossPerc = crossPerc
+        self.mutePerc = mutePerc
+        if self.popSize % 2 == 1:  # if user puts in an odd population size, make it even
+            print("Making population size even:")
+            self.popSize += 1
+        self.currStates = []
+        for i in range(self.popSize):
+            nextState = self.stateGen
+            self.currStates.append(nextState)
+        self.maxFit = self.currStates[0].getMaxValue()
         self.count = 0
-        if verbose:
-            print("============= START ==============")
 
     def getCount(self):
         """Returns the current count."""
@@ -231,90 +263,54 @@ class GASearcher:
 
     def getCurrState(self):
         """Returns the current state."""
-        return self.currState
+        return self.currStates[0]
 
     def getCurrValue(self):
         """Returns the value currently associated with the current state."""
-        return self.currValue
-
+        return self.currStates[0].getMaxValue()
 
     def run(self):
-        """Perform the hill-climbing algorithm, starting with the given start state and going until a local maxima is
-        found or the maximum rounds is reached"""
-        status = None
-
-        while self.currValue < self.maxValue and self.count < self.maxRounds:
-            status = self.step()
-
-            if status == 'optimal' or status == 'local maxima':
-                break
-
-        if verbose:
-            print("============== FINAL STATE ==============")
-            print(self.currState)
-            print("   Number of steps =", self.count)
-            if status == 'optimal':
-                print("  FOUND PERFECT SOLUTION")
-        return self.currValue, self.maxValue, self.count
-
+        foundOptimal = False
+        overallBest = self.currStates[0]
+        while (not foundOptimal) and self.count < self.maxGenerations:
+            self.step()
 
     def step(self):
-        """Runs one step of hill-climbing, generates children and picks the best one, returning it as its value. Also returns
-        a second value that tells if the best child is optimal or not."""
+        foundOptimal = False
+        overallBest = self.currStates[0]
         self.count += 1
-        if verbose:
-            print("--------- Count =", self.count, "---------")
-            print(self.currState)
-        neighs = self.currState.randomNeighbors(8)    # TODO: Modify the number of neighbors here
-        bestNeigh = self.findBestNeighbor(neighs)
-        nextValue = bestNeigh.getValue()
-        self.currState = bestNeigh
-        self.currValue = nextValue
-        if nextValue == self.maxValue:
-            return 'optimal'
-        if nextValue >= self.currValue:
-            if verbose:
-                print("Best neighbor:")
-                print(bestNeigh)
-            return 'keep going'
+        fits = [state.getValue() for state in self.currStates]
+        if self.maxFit in fits:  # we have an optimal solution
+            pos = fits.index(self.maxFit)
+            bestOne = self.currStates[pos]
+            foundOptimal = True
         else:
-            # best is worse than current
-            return 'local maxima'
-
-
-    def findBestNeighbor(self, neighbors):
-        """Given a list of neighbors and values, find and return a neighbor with
-        the best value. If there are multiple neighbors with the same best value,
-        a random one is chosen"""
-        startBest = neighbors[0]
-        print(startBest.ruleString)
-        bestValue = startBest.getValue()
-        bestNeighs = [startBest]
-        for neigh in neighbors[1:]:
-            value = neigh.getValue()
-            if value > bestValue:
-                bestNeighs = [neigh]
-                bestValue = value
-            elif value == bestValue:
-                bestNeighs.append(neigh)
-        bestNeigh = random.choice(bestNeighs)
-        return bestNeigh
-class GASearcher(object):
-
-    def __init__(self, stateGen, popSize=30, maxGenerations=2000, crossPerc=0.8, mutePerc=0.01):
-        self.stateGen = stateGen
+            bestLoc = fits.index(max(fits))
+            bestOne = self.currStates[bestLoc]
+            parentPool = self.selectParents(self.currStates, fits)
+            currStates = self.mateParents(parentPool, self.crossPerc, self.mutePerc)
+        if bestOne.getValue() > overallBest.getValue():
+            overallBest = bestOne
+        if verbose:
+            print("============== GOAL ==============")
+            print("  Last generation best one:")
+            print(bestOne)
+            print("  Overall best discovered:")
+            print(overallBest)
+            print("   Number of steps =", self.count)
+        return bestOne.getValue(), self.maxFit, self.count
 
     # TODO: Implement the rest of the genetic alg from the localSearch.py file in the Queens folder
 
-    def selectParents(states, fitnesses):
+    def selectParents(self, states, fitnesses):
         """given a set of states, repeatedly select parents using roulette selection"""
         parents = []
         for i in range(len(states)):
-            nextParentPos = rouletteSelect(fitnesses)
+            nextParentPos = self.rouletteSelect(fitnesses)
             parents.append(states[nextParentPos])
         return parents
 
-    def mateParents(parents, crossoverPerc, mutationPerc):
+    def mateParents(self, parents, crossoverPerc, mutationPerc):
         """Given a set of parents, pair them up and cross them together to make
         new kids"""
         newPop = []
@@ -336,7 +332,7 @@ class GASearcher(object):
                 newPop[i] = nextOne.makeRandomMove()
         return newPop
 
-    def rouletteSelect(valueList):
+    def rouletteSelect(self, valueList):
         """takes in a list giving the values for a set of entities.  It randomly
     selects one of the positions in the list by treating the values as a kind of
     probability distribution and sampling from that distribution.  Each entity gets
